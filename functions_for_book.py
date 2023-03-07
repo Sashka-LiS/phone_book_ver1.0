01ab2fcfcceaa91704422f58c0c923483032a5bf
Alexandr Lisimenko <lisimenko.alexander@yandex.ru>
2023-03-07 14:46:06 +0300

    update function_for_book.py

--- functions_for_book.py
+++ functions_for_book.py
@@ -48,20 +48,6 @@ def get_db():
     return db_connection
 
 
-# def db_cursor(phone_book=get_db()):
-#     cursor = phone_book.cursor()
-#     return cursor
-
-
-def execute_add_del(query, values, phone_book=get_db()):
-    cursor = phone_book.cursor()
-    cursor.execute(query, values)
-    phone_book.commit()
-
-
-def cursor_execute(query, values, phone_book=get_db()):
-    cursor = phone_book.cursor()
-    return cursor.execute(query, values)
 
 
 def is_valid_email(email, phone_book=get_db()):
@@ -86,215 +72,168 @@ def is_valid_number(number):
     return True
 
 
-def add_contact():
-
-    try:
-        surname = input('Фамилия --> ')
-        name = input('Имя --> ')
-        while not is_valid_name(name):
-            print('Это поле не может быть пустым.')
-            name = input('Имя --> ')
-
-        father_name = input('Отчество --> ')
-        email = input('Email --> ')
-        while not is_valid_email(email):
-            print('Такой email уже есть в телефонной книге. Попробуте еще раз.')
-            email = input('Email --> ')
-
-        number = input('Номер телефона --> ')
-        while not is_valid_number(number):
-            print('Формат номера должен быть "+7..." или "8..."')
-            number = input('Номер телефона --> ')
-
-        home_number = input('Домашний номер --> ')
-        work_number = input('Рабочий номер --> ')
-
-        values_for_contacts = [surname, name, father_name, email]
-        values_for_numbers = [number, home_number, work_number]
-
-        execute_add_del('INSERT INTO контакты(фамилия, имя, отчество, email) VALUES (?, ?, ?, ?);', values_for_contacts)
-        execute_add_del('INSERT INTO номера (номер, домашний, рабочий) VALUES (?, ?, ?);', values_for_numbers)
-    except sq.Error as e:
-        print('ERROR: ', e)
-    
-    result = '\nКонтакт добавлен.'
-    return result
+def is_valid_value(value):
+     empty_string = ''
+     if value == empty_string:
+          return False
+     return True
 
 
+def add_contact(values_contacts, values_numbers, phone_book=get_db()):
+        cursor = phone_book.cursor()
+        cursor.execute('INSERT INTO контакты(фамилия, имя, отчество, email) VALUES (?, ?, ?, ?);', values_contacts)
+        cursor.execute('INSERT INTO номера (номер, домашний, рабочий) VALUES (?, ?, ?);', values_numbers)
+        phone_book.commit()
+        return True #Пусть возвращается id контакта.
 
-def show_book():
 
-    try:
-        query = ('''SELECT фамилия, имя, отчество, email, номера.номер, номера.домашний, номера.рабочий 
-                          FROM контакты 
-                          JOIN номера ON контакты.id_contact = номера.id_number;
-                          ''')  
-        cursor = db_cursor(query)
-        contacts = cursor.fetchall()    
-    except sq.Error as e:
-        print('ERROR: ', e)
-    finally:
-        cursor.close()
+def show_book(phone_book=get_db()):
+    cursor = phone_book.cursor()
+    cursor.execute('''SELECT фамилия, имя, отчество, email, номера.номер, номера.домашний, номера.рабочий 
+                      FROM контакты 
+                      JOIN номера ON контакты.id_contact = номера.id_number;
+                      ''')
+    print('*' * 80)
+    pprint(cursor.fetchall())
+    print('*' * 80)
+    return True
+
+
+def del_contact(names_del, phone_book=get_db()):
         
-
-    return contacts
-
-
-def del_contact():
-
-    try:
-        name = input('Имя контакта для удаления контакта --> ')
-        names_for_del = [name]
-        ('''SELECT id_contact, фамилия, имя, отчество, номера.номер, номера.домашний, номера.рабочий 
+        cursor = phone_book.cursor()
+        cursor.execute('''SELECT id_contact, фамилия, имя, отчество, номера.номер, номера.домашний, номера.рабочий 
                           FROM контакты JOIN номера ON контакты.id_contact = номера.id_number 
-                          WHERE имя = ?;''', names_for_del)
-        print('-' * 20)
-        pprint(db_cursor().fetchall()) #!!!!!!!!!!
-        print('-' * 20)
+                          WHERE имя = ?;''', names_del)
+        
+        print('*' * 80)
+        pprint(cursor.fetchall())
+        print('*' * 80)
+
         select_id = int(input('Выбери ID контакта для удаления --> '))
         id_for_del = [select_id]
-        execute_add_del('PRAGMA foreign_keys=on')
-        execute_add_del('DELETE FROM контакты WHERE id_contact = ?', id_for_del)
-    except sq.Error as e:
-        print('ERROR: ', e)
 
-    result = '\nКонтакт удален.'
-    return result
+        cursor.execute('PRAGMA foreign_keys=on')
+        cursor.execute('DELETE FROM контакты WHERE id_contact = ?', id_for_del)
+        phone_book.commit()
+        return True
 
 
-def find_contact():
+def search_by_surname(surname, phone_book=get_db()):
+    cursor = phone_book.cursor()
+    value_for_finde = ['%' + surname + '%']
+    cursor.execute('''SELECT фамилия, имя, отчество, номера.номер, номера.домашний, номера.рабочий
+                      FROM контакты
+                      JOIN номера ON контакты.id_contact = номера.id_number
+                      WHERE фамилия LIKE ?''', value_for_finde)
 
-    print('''
-Где искать контакт?
+    print('*' * 80)
+    pprint(cursor.fetchall())     
+    print('*' * 80)
 
-1 - Фамилия
-2 - Имя
-3 - Отчество
-4 - Основной номер
-5 - Домашний номер
-6 - Рабочий
-0 - Отмена''')
+    return True
 
-    my_response = input('--> ')
 
-    if my_response == '1':
-        surname = input('Фамилия или часть фамилии контакта --> ')
-        value_for_find = ['%' + surname + '%']
-        try:
-            phone_book = sq.connect(name_db)
-            cursor = phone_book.cursor()
-            cursor.execute('''SELECT фамилия, имя, отчество, номера.номер, номера.домашний, номера.рабочий
-                              FROM контакты
-                              JOIN номера ON контакты.id_contact = номера.id_number
-                              WHERE фамилия LIKE ?
-                              ''', value_for_find)
-            contacts = cursor.fetchall()
-        except sq.Error as e:
-            print('ERROR: ', e)
-        finally:
-            cursor.close()
-            phone_book.close()        
-        return contacts
+def search_by_name(name, phone_book=get_db()):
+    cursor = phone_book.cursor()
+    value_for_finde = ['%' + name + '%']
+    cursor.execute('''SELECT фамилия, имя, отчество, номера.номер, номера.домашний, номера.рабочий
+                      FROM контакты
+                      JOIN номера ON контакты.id_contact = номера.id_number
+                      WHERE имя LIKE ?''', value_for_finde)
 
-    elif my_response == '2':
-        name = input('Имя или часть имени контакта --> ')
-        value_for_find = ['%' + name + '%']
-        try:
-            phone_book = sq.connect(name_db)
-            cursor = phone_book.cursor()
-            cursor.execute('''SELECT фамилия, имя, отчество, номера.номер, номера.домашний, номера.рабочий
-                              FROM контакты
-                              JOIN номера ON контакты.id_contact = номера.id_number
-                              WHERE имя LIKE ?
-                              ''', value_for_find)            
-            contacts = cursor.fetchall()
-        except sq.Error as e:
-            print('ERROR: ', e)
-        finally:
-            cursor.close()
-            phone_book.close()
-        return contacts
-    
-    elif my_response == '3':
-        father_name = input('Отчество или часть отчества контакта --> ')
-        value_for_find = ['%' + father_name + '%']
-        try:
-            phone_book = sq.connect(name_db)
-            cursor = phone_book.cursor()
-            cursor.execute('''SELECT фамилия, имя, отчество, номера.номер, номера.домашний, номера.рабочий
-                              FROM контакты
-                              JOIN номера ON контакты.id_contact = номера.id_number
-                              WHERE отчество LIKE ?
-                              ''', value_for_find)            
-            contacts = cursor.fetchall()
-        except sq.Error as e:
-            print('ERROR: ', e)
-        finally:
-            cursor.close()
-            phone_book.close()
-        return contacts
-    
-    elif my_response == '4':
-        number = input('Номер или часть номера контакта --> ')
-        value_for_find = ['%' + number + '%']
-        try:
-            phone_book = sq.connect(name_db)
-            cursor = phone_book.cursor()
-            cursor.execute('''SELECT фамилия, имя, отчество, номера.номер, номера.домашний, номера.рабочий
-                              FROM контакты
-                              JOIN номера ON контакты.id_contact = номера.id_number
-                              WHERE номер LIKE ?
-                              ''', value_for_find)
-            contacts = cursor.fetchall()
-        except sq.Error as e:
-            print('ERROR: ', e)
-        finally:
-            cursor.close()
-            phone_book.close()
-        return contacts
+    print('*' * 80)
+    pprint(cursor.fetchall())     
+    print('*' * 80)
 
-    elif my_response == '5':
-        home_number = input('Домашний номер или часть домашнего номера контакта --> ')
-        value_for_find = ['%' + home_number + '%']
-        try:
-            phone_book = sq.connect(name_db)
-            cursor = phone_book.cursor()
-            cursor.execute('''SELECT фамилия, имя, отчество, номера.номер, номера.домашний, номера.рабочий
-                              FROM контакты
-                              JOIN номера ON контакты.id_contact = номера.id_number
-                              WHERE домашний LIKE ?
-                              ''', value_for_find)            
-            contacts = cursor.fetchall()
-        except sq.Error as e:
-            print('ERROR: ', e)
-        finally:
-            cursor.close()
-            phone_book.close()
-        return contacts
- 
-    elif my_response == '6':
-        work_number = input('Рабочий номер или часть рабочего номера контакта --> ')
-        value_for_find = ['%' + work_number + '%']
-        try:
-            phone_book = sq.connect(name_db)
-            cursor = phone_book.cursor()
-            cursor.execute('''SELECT фамилия, имя, отчество, номера.номер, номера.домашний, номера.рабочий
-                              FROM контакты
-                              JOIN номера ON контакты.id_contact = номера.id_number
-                              WHERE рабочий LIKE ?
-                              ''', value_for_find)            
-            contacts = cursor.fetchall()
-        except sq.Error as e:
-            print('ERROR: ', e)
-        finally:
-            cursor.close()
-            phone_book.close()
-        return contacts
+    return True
+
+
+def search_by_father_name(father_name, phone_book=get_db()):
+    cursor = phone_book.cursor()
+    value_for_finde = ['%' + father_name + '%']
+    cursor.execute('''SELECT фамилия, имя, отчество, номера.номер, номера.домашний, номера.рабочий
+                      FROM контакты
+                      JOIN номера ON контакты.id_contact = номера.id_number
+                      WHERE отчество LIKE ?''', value_for_finde)
+
+    print('*' * 80)
+    pprint(cursor.fetchall())     
+    print('*' * 80)
+
+    return True
+
+
+def search_by_number(number, phone_book=get_db()):
+    cursor = phone_book.cursor()
+    value_for_finde = ['%' + number + '%']
+    cursor.execute('''SELECT фамилия, имя, отчество, номера.номер, номера.домашний, номера.рабочий
+                      FROM контакты
+                      JOIN номера ON контакты.id_contact = номера.id_number
+                      WHERE номер LIKE ?''', value_for_finde)
+
+    print('*' * 80)
+    pprint(cursor.fetchall())     
+    print('*' * 80)
+
+    return True
+
+def search_by_home_number(home_number, phone_book=get_db()):
+    cursor = phone_book.cursor()
+    value_for_finde = ['%' + home_number + '%']
+    cursor.execute('''SELECT фамилия, имя, отчество, номера.номер, номера.домашний, номера.рабочий
+                      FROM контакты
+                      JOIN номера ON контакты.id_contact = номера.id_number
+                      WHERE домашний LIKE ?''', value_for_finde)
+
+    print('*' * 80)
+    pprint(cursor.fetchall())     
+    print('*' * 80)
+
+    return True
+
+
+def search_by_work_number(work_number, phone_book=get_db()):
+    cursor = phone_book.cursor()
+    value_for_finde = ['%' + work_number + '%']
+    cursor.execute('''SELECT фамилия, имя, отчество, номера.номер, номера.домашний, номера.рабочий
+                      FROM контакты
+                      JOIN номера ON контакты.id_contact = номера.id_number
+                      WHERE рабочий LIKE ?''', value_for_finde)
+
+    print('*' * 80)
+    pprint(cursor.fetchall())     
+    print('*' * 80)
+
+    return True
+#     elif response_finde == '6':
+#         work_number = input('Рабочий номер или часть рабочего номера контакта --> ')
+#         value_for_find = ['%' + work_number + '%']
+#         cursor.execute('''SELECT фамилия, имя, отчество, номера.номер, номера.домашний, номера.рабочий
+#                           FROM контакты
+#                           JOIN номера ON контакты.id_contact = номера.id_number
+#                           WHERE рабочий LIKE ?
+#                           ''', value_for_find)            
         
-    elif my_response == '0':
-        result = 'Отмена'
-        return result
+#         print('*' * 80)
+#         pprint(cursor.fetchall())
+#         print('*' * 80)
     
-    else:
-        print('Такой команды нет.')
-        find_contact()
+#     elif response_finde == '0':
+#         pass
+    
+#     else:
+#         print('Такой команды нет.')
+#         print('''
+# \nГде искать контакт?
+# 1 - Фамилия
+# 2 - Имя
+# 3 - Отчество
+# 4 - Основной номер
+# 5 - Домашний номер
+# 6 - Рабочий
+# 0 - Отмена
+#         ''')
+#         response_finde = input(' --> ')
+#         find_contact(response_finde, phone_book=get_db())
+#     return True
